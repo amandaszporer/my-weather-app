2e9025372a72a7d17e2f500fa9f3f0d410c0787d
amandaszporer <amszporer@gmail.com>
2020-11-12 16:15:26 +0200

    Improved Conversion Celsius/Fahrenheit

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 let now = new Date()
 let apiKey = "5245c70173752e9c7a1ea0e23d87c083"
+let celsiusTemp = null
+let feelsLikeCelsius = null
+let maxTempCelsius = null
+let minTempCelsius = null
 
 function updateTime (){
   let time = document.querySelector("#time")
@@ -34,10 +38,14 @@ function updateTime (){
     document.querySelector("#max-temp").innerHTML = Math.round(response.data.main.temp_max)
     document.querySelector("#feels-like").innerHTML = Math.round(response.data.main.feels_like)
     document.querySelector("#description").innerHTML = response.data.weather[0].description
+    celsiusTemp = response.data.main.temp
+    feelsLikeCelsius = response.data.main.feels_like
+    maxTempCelsius = response.data.main.temp_max
+    minTempCelsius = response.data.main.temp_min
+   
   }
 
   function search(city){
-
     let apiEndPoint = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&&units=metric`
     axios.get(apiEndPoint).then(updateTemperature)
   }
@@ -50,45 +58,44 @@ function updateTime (){
   
   searchCity.addEventListener("submit", handleSubmit)
   
-  search("Tel Aviv")
-  
+  let fahrenheitTemperature = document.querySelector("#fahrenheit-link")
 
-let farenheit = document.querySelector("#farenheit-temp")
+  function displayFahrenheitTemperarute(event){
+    event.preventDefault()
+    document.querySelector("#current-temp").innerHTML = `${Math.round(((celsiusTemp) * 9/5)+32)} `
+    document.querySelector("#feels-like").innerHTML = `${Math.round(((feelsLikeCelsius) * 9/5)+32)}`
+    document.querySelector("#min-temp").innerHTML = `${Math.round(((minTempCelsius) * 9/5)+32)}`
+    document.querySelector("#max-temp").innerHTML = `${Math.round(((maxTempCelsius) * 9/5)+32)}`
+  }
 
-function convertFarenheit (event){
-  event.preventDefault()
-  let tempFarenheit = document.querySelector("#current-temp")
-  let temperature =  tempFarenheit.innerHTML
-  tempFarenheit.innerHTML = `${Math.round((temperature * 9) / 5 + 32)} `
-}
+  fahrenheitTemperature.addEventListener("click", displayFahrenheitTemperarute)
 
-  farenheit.addEventListener("click", convertFarenheit)
+  let celsiusTemperature = document.querySelector("#celsius-link")
 
-let celsius = document.querySelector("#celsius-temp")
+  function displayCelsiusTemperature(event){
+    event.preventDefault()
+    document.querySelector("#current-temp").innerHTML = `${Math.round(celsiusTemp)} `
+    document.querySelector("#feels-like").innerHTML = `${Math.round(feelsLikeCelsius)}`
+    document.querySelector("#min-temp").innerHTML = `${Math.round(minTempCelsius)}`
+    document.querySelector("#max-temp").innerHTML = `${Math.round(maxTempCelsius) }`
+  }
 
-function convertCelsius (event){
-  event.preventDefault()
-  let tempCelsius = document.querySelector("#current-temp")
-  let temperature = tempCelsius.innerHTML
-  tempCelsius.innerHTML = `${Math.round((temperature -32) * 5/9)} `}
-celsius.addEventListener("click", convertCelsius)
-
+  celsiusTemperature.addEventListener("click", displayCelsiusTemperature)
 
 function handlePosition(position){
   let lat = position.coords.latitude
   let lon = position.coords.longitude
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&&units=metric`
   axios.get(apiUrl).then(updateTemperature)
-
 }
-
-
 
 function getLocation(event){
   navigator.geolocation.getCurrentPosition(handlePosition)
 }
 
-
-
 let currentLocation = document.querySelector("#current-location")
 currentLocation.addEventListener("click", getLocation)
+
+
+
+getLocation();
