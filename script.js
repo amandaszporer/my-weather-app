465a12f7a449cc73f18075f2bc57878c5626772d
amandaszporer <amszporer@gmail.com>
2020-11-14 00:10:55 +0200

    Implemented hourly forecast

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let feelsLikeCelsius = null
 let maxTempCelsius = null
 let minTempCelsius = null
 
+
 let searchCity = document.querySelector("#search-city");
 
   function formatDate (timestamp){
@@ -36,7 +37,6 @@ let searchCity = document.querySelector("#search-city");
     document.querySelector("#description").innerHTML = response.data.weather[0].description
     document.querySelector("#day").innerHTML = formatDate(response.data.dt*1000)
     document.querySelector("#time").innerHTML = formatHours(response.data.dt*1000)
-    
     document.querySelector("#current-icon").setAttribute ("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png` )
     celsiusTemp = response.data.main.temp
     feelsLikeCelsius = response.data.main.feels_like
@@ -48,25 +48,25 @@ let searchCity = document.querySelector("#search-city");
     let forecastElement = document.querySelector("#forecast")
     forecastElement.innerHTML = null
     let forecast = null
-    for (let index = 0; index < 2; index++) {
+    for (let index = 0; index < 6; index++) {
       forecast = response.data.list[index]
       forecastElement.innerHTML += `
       <div class="col-2">
-                  <h3>
-                  ${formatHours(forecast.dt)}
+                  <h3 class="forecast-info">
+                  ${formatHours(forecast.dt*1000)}
                   </h3>
                   <img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png" id="forecast-image">
-                  <div>
-                  <strong>${Math.round(forecast.main.temp_max)}° </strong> ${Math.round(forecast.main.temp_min)}°</div>
+                  <div class="forecast-info">
+                  <strong><span id="max-forecast">${Math.round(forecast.main.temp_max)}</span>° </strong> <span id="min-forecast">${Math.round(forecast.main.temp_min)}°</span></div>
                  
-              </div>
-      `
-      
-    }
-    
-
-    
+              </div>`
+    } 
   }
+    function displayForecastByGeolocation (response){
+      let city = response.data.name
+      let apiEndPointForecast = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&&units=metric`
+      axios.get(apiEndPointForecast).then(displayForecast)
+    }
 
 
   function search(city){
@@ -113,8 +113,7 @@ function handlePosition(position){
   let lon = position.coords.longitude
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&&units=metric`
   axios.get(apiUrl).then(updateTemperature)
-
-
+  axios.get(apiUrl).then(displayForecastByGeolocation)
 }
 
 function getLocation(event){
@@ -123,7 +122,5 @@ function getLocation(event){
 
 let currentLocation = document.querySelector("#current-location")
 currentLocation.addEventListener("click", getLocation)
-
-
 
 search("Tel Aviv");
